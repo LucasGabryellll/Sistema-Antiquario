4a1fb6317c9b901b43ac4acf6e80d3b02737122a
LucasGabryellll <gabryelmonteiro1@gmail.com>
2021-12-24 16:24:34 -0300

    Adding Class TipoErro

---
+++ sistemaAntiquario/sistema/model/TipoErro.java
@@ -0,0 +1,11 @@
+package sistema.model;
+
+public class TipoErro {
+	
+	public static final String NULL_PRODUTO = "Dados de Produto Inválidos";
+	public static final String EXISTE_PRODUTO = "Produto já existe";
+	public static final String NAO_EXISTE_PRODUTO = "Produto não existe";
+	public static final String QUANTIDADE_ITEM_ERROR = "Quantidade indisponível";
+	public static final String NAO_EXISTE_IMAGEM = "Erro ao c...";
+	
+}
